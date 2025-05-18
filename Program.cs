fc3542a6effcc146c83d30372348e2fa090ec810
fartb <fartbreather228@gmail.com>
2025-05-19 00:02:07 +0300

    добавлены комментарии и вывод домика из символов

--- Program.cs
+++ Program.cs
@@ -37,6 +37,8 @@ TeamLeader (Бригадир).
 
 */
 
+// переменная бригады строителей с инициализацией
+
 Team new_building = new Team
 {
     _builders = new List<Builder>
@@ -52,12 +54,18 @@ Team new_building = new Team
     _house = new House()
 };
 
+// пока дом не закончен, посылается рабочий 
+// с силой 1 и выводится сообщение о отправке на обьект
 while(new_building.Build(1) != false)
 {
     WriteLine("\n\nBuilder with strength 1 been sent to build the house.");
 }
 
+// по завершению стройки составляется отчёт
+
 new_building.Report();
+
+// ИНТЕРФЕЙСЫ
 
 public interface IPart
 {
@@ -70,6 +78,8 @@ public interface IWorker
 {
     string _name { get; }
 }
+
+// ЧАСТИ ДОМА
 
 public class Basement : IPart
 {
@@ -102,6 +112,8 @@ public class Door : IPart
     public bool _isdone { set; get; } = false;
 }
 
+// РАБОЧИЕ
+
 public class Builder : IWorker
 {
     public string _name { set; get; }
@@ -112,41 +124,67 @@ public class Team_Leader : IWorker
     public string _name { set; get; }
 }
 
+// КЛАСС БРИГАДЫ РАБОЧИХ
+
 public class Team
 {
+    // контейнер рабочих
     public List<Builder> _builders { set; get; }
+    // бригадир
     public Team_Leader _leader { set; get; }
+    // дом необходимый к постройке
     public House _house {  set; get; }
 
+    // метод стройки дома
     public bool Build(int req_str)
     {
+        // пробегаемся по массиву в классе дома
         for (int i = 0; i < _house.parts.Length; i++) 
         {
+            // если текущий парт ещё не построен
+            // переходим в условие
             if (_house.parts[i]._isdone == false)
             {
+                // пока время необходимое для постройки
+                // не истекло, рабочий продолжает пахать
                 while (_house.parts[i]._time > 0)
                 {
+                    // вывод о том, какой рабочий и какую часть делает
                     WriteLine($"\nBuilder {_builders[req_str - 1]._name} is building the object {_house.parts[i]._name}..");
 
+                    // отнимаем от времени парта сделанную часть
                     _house.parts[i]._time -= _builders[req_str - 1]._strength;
                 }
 
+                // по окончанию постройки ставим готовность на тру
                 _house.parts[i]._isdone = true;
 
+                // возвращение тру, извещающего о выполненной работе
                 return true;
             }
         }
 
+        // вывод радостного крика рабочего если
+        // вся постройка уже завершена но
+        // его зачем-то послали на объект
         WriteLine("\nKaloo kalay no work today!");
 
+        // возвращение фолс если работа не потребовалась
         return false;
     }
 
+    // метод создания отчёта
     public void Report()
     {
+        // переменная для отлова значения готовности
+        bool if_smt_not_ready = false;
+        // вывод имени лидера бригады 
         WriteLine($"\n\n{_leader._name}'s report says:\n");
+        //пробег по всем партам дома
         for (int i = 0; i < _house.parts.Length; i++)
         {
+            // если готовность парта на тру
+            // то это выводится в консоль и наоборот
             if (_house.parts[i]._isdone == true)
             {
                 WriteLine($"\nObject {_house.parts[i]._name} is done!");
@@ -154,10 +192,25 @@ public class Team
             else
             {
                 WriteLine($"\nObject {_house.parts[i]._name} isn't done yet.");
+
+                // если всё-таки какая-то часть не готова
+                // переменной присваивается тру
+                if_smt_not_ready = true;
             }
+        }
+
+        // вывод домика из символов и поздравления по завершению
+        // в случае если все части дома успешно построены к моменту отчёта
+        if( !if_smt_not_ready )
+        {
+            WriteLine("\n\n        `'::.\r\n    _________H ,%%&%,\r\n   /\\     _   \\%&&%%&%\r\n  /  \\___/^\\___\\%&%%&&\r\n  |  | []   [] |%\\Y&%'\r\n  |  |   .-.   | ||  \r\n~~@._|@@_|||_@@|~||~~~~~~~~~~~~~\r\n     `\"\"\") )\"\"\"`");
+
+            WriteLine("\n\nCongrats! Your house is ready!");
         }
     }
 }
+
+// КЛАСС ДОМА
 
 public class House
 {
